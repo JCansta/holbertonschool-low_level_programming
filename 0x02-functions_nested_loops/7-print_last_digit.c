9ccc768b1ee229bd0fbf1049bbd96e7d21d03741
Jaime Castro <jaime.castro03@icloud.com>
2020-06-16 22:21:10 -0500

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,13 +12,12 @@ int print_last_digit(int n)
 {
 	if (n >= 0)
 	{
-	n = (n % 10);
-	_putchar(n + '0');
+		n = (n % 10);
 	}
 	else
 	{
-		n = (-n % 10);
-		_putchar(n + '0');
+		n = -1 * (n % 10);
 	}
+	_putchar(n + '0');
 	return (n);
 }
