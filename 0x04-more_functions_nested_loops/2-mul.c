3a020ae8a333dbfa307a81ea567547272fe99fd8
Jaime Castro <jaime.castro03@icloud.com>
2020-06-18 17:09:19 -0500

    task 0,1,2,3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ *int mul - chekc if char is upper
+ *@a: digit to multiplie
+ *@b: digit to multiplie
+ *Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int mul = a * b;
+
+	return (mul);
+}
+
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *int mul - chekc if char is upper
+ *@a: digit to multiplie
+ *@b: digit to multiplie
+ *Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i, '0');
+	}
+	_putchar('\n');
+}
