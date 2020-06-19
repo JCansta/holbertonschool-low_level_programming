d774884a8245b626fc48db36c815101bb13eb08c
Jaime Castro <jaime.castro03@icloud.com>
2020-06-18 20:45:35 -0500

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ *print_triangle - fizzbuzz
+ *@size: triangle size
+ *Return: Always 0.
+ */
+
+void print_triangle(int size)
+{
+	int i, j, cont;
+
+	for (i = 0; i < size; i++)
+	{
+		cont = size;
+		for (j = 0; j < size; j++)
+		{
+			if (cont <= (i+1))
+				_putchar('#');
+			else
+				_putchar(' ');
+
+			cont--;
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
