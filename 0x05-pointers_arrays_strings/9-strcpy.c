052d90764e8cca1477f006777a2b69ccae35d77a
Jaime Castro <jaime.castro03@icloud.com>
2020-06-30 23:32:17 -0500

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,10 +10,13 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
+	if (*src == '\0')
+		return(dest);
 	do {
 		dest[i] = src[i];
 		i++;
 	} while (src[i] != '\0');
+	dest[i] = '\0';
 
 
 	return (dest);
