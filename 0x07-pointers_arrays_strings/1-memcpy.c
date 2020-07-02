05115103c0f4f50f3de35075f8b6c1599856dc9a
Jaime Castro <jaime.castro03@icloud.com>
2020-07-02 14:07:11 -0500

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,7 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	char *p = dest;
 	unsigned int i;
 
-	for (i = 0; i < n && *src != '\0'; i++)
+	for (i = 0; i < n; i++)
 	{
 		*p = *src;
 		p++;
