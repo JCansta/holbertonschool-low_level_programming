f5a0600f7438027418b62c8ae23eed0727210d46
Jaime Castro <jaime.castro03@icloud.com>
2020-07-05 19:59:21 -0500

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+/**
+* main-print the name of the file,
+*@argc: how long is the function
+*@argv: porinter to the array of the function.
+*Return: the return of finish file
+*/
+
+int main(int argc, char *argv[argc])
+{
+	int mult, x[2], y = 0;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	argc--;
+	argv++;
+	while (argc--)
+	{
+		
+		x[y] = atoi(*argv);
+		y++;
+		argv++;
+	}
+	mult = x[0] * x[1];
+	printf("%d\n", mult);
+	return (0);
+}
