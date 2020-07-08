1063fab512fe173a2de60281f4ace55d547ba158
Jaime Castro <jaime.castro03@icloud.com>
2020-07-07 22:16:47 -0500

    task 1 with old functions

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,7 +1,45 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+/**
+ * _strcpy - wenas.
+ * @dest: bring a direction.
+ * @src: bring a int.
+ * Return: Always 0.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	if (*src == '\0')
+		return (dest);
+	do {
+		dest[i] = src[i];
+		i++;
+	} while (src[i] != '\0');
+	dest[i] = '\0';
+
+
+	return (dest);
+}
+
+/**
+ *_strlen - chekc if char is upper
+ *@s: give the pointing direction
+ *Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
 /* more headers goes there */
 /**
 *_strdup - return a pointer to a new allocated space
@@ -11,13 +49,13 @@
 
 char *_strdup(char *str)
 {
-	char *dst = malloc(strlen(str) + 1);
+	char *dst = malloc(_strlen(str) + 1);
 
 	if (dst == NULL)
 	{
 		return (NULL);
 	}
-	strcpy(dst, str);
+	_strcpy(dst, str);
 
 	return (dst);
 }
