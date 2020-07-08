35d46cdcddece0280c1280d28e614fd071e0c868
Jaime Castro <jaime.castro03@icloud.com>
2020-07-08 13:46:41 -0500

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,11 +14,12 @@ char *create_array(unsigned int size, char c)
 	char *p = (char *)malloc(size * sizeof(char));
 	unsigned int i;
 
+	if (size == '\0')
+		return ('\0');
+
 	for (i = 0; i <= size; i++)
 		p[i] = c;
 
-	if (size == '\0')
-		return ('\0');
 	if (size == 0)
 	{
 		return ('\0');
