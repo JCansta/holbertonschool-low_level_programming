618a4a6a2cab9db920d7458b2c95bd9c2bd30cf4
Jaime Castro <jaime.castro03@icloud.com>
2020-07-08 15:19:03 -0500

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,59 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *_strlen - chekc if char is upper
+ *@s: give the pointing direction
+ *Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+
+
+/**
+ * str_concat - concatenate 2 strings
+ * @s1: first string
+ * @s2: second string.
+ * Return: concatenate string.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int sum, i = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	sum = _strlen(s1) + _strlen(s2);
+
+	char *p = (char *) malloc((sum * sizeof(char)) + 1);
+	int d = 0;
+
+	while (i <= sum)
+	{
+		if (i < _strlen(s1))
+		{
+			p[i] = s1[i];
+		}
+		else
+		{
+			p[i] = s2[d];
+			d++;
+		}
+
+		i++;
+	}
+	return (p);
+}
