03d87e817f53c8b6976cd44e11b79c3844d98d80
Jaime Castro <jaime.castro03@icloud.com>
2020-07-08 22:16:48 -0500

    task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *alloc_grid - create a 2D array with malloc
+ *@width: filas
+ *@height: columnas
+ *Return: Always 0.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **p;
+	int i = 0, j = 0;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	p = (int **) malloc(width * sizeof(int *));
+	
+	if (p == NULL)
+		return (NULL);
+
+	while (i < width)
+	{
+		p[i] = (int *)malloc(height * sizeof(int));
+		if (p[i] == NULL)
+			return (NULL);
+		i++;
+	}
+	
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < height; j++)
+		{
+			p[i][j] = 0;
+		}
+	}
+	
+	return (p);
+}
