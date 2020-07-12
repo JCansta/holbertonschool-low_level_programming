ca889fcb4d71d4a2f54e1b44786089461bc9debb
Jaime Castro <jaime.castro03@icloud.com>
2020-07-11 23:35:05 -0500

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,39 +33,29 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
-
 	l1 = _strlen(s1);
 	l2 = _strlen(s2);
-
 	if (n >= l2)
 		n = l2;
-
 	p = malloc((sizeof(char) * l1) + (sizeof(char) * n) + 1);
-
 	if (p == NULL)
 		return (NULL);
-
 	s = p;
-
-
-        while (*s1 != '\0')
-        {
-                *s = *s1;
-                s++;
-                s1++;
-        }
-        while (i < n)
-        {
-                *s = *s2;
-                s++;
-                s2++;
-                i++;
-        }
-
-        *s = '\0';
-
+	while (*s1 != '\0')
+	{
+		*s = *s1;
+		s++;
+		s1++;
+	}
+	while (i < n)
+	{
+		*s = *s2;
+		s++;
+		s2++;
+		i++;
+	}
+	*s = '\0';
 	return (p);
 }
