cba03eac0794ed9f1e377b749ae1b6c03a1e2941
Jaime Castro <jaime.castro03@icloud.com>
2020-07-14 09:49:28 -0500

    changing betty, I hate it so much

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,7 +20,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 /**
   *dog_t - es el nuevo nombre que se le da a dog luego de usar typedef
   */
-typedef struct dog dog_t; 
+typedef struct dog dog_t;
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
