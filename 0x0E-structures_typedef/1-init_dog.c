6dd1c8e7c2eb31028b96ade17d11cbabeea6768d
Jaime Castro <jaime.castro03@icloud.com>
2020-07-13 21:16:58 -0500

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+* init_dog - print the name of the file,
+*@dog: structure dog.
+*@name: name of the dog.
+*@age: age of the dog.
+*@owner: name of the owner.
+*Return: the return of finish file
+*/
+
+void init_dog(struct dog *d; char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return (NULL);
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
