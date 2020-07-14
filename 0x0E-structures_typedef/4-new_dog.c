a503f13b6db88c0af38ef7d86af3e9b0eaad8c0d
Jaime Castro <jaime.castro03@icloud.com>
2020-07-13 23:43:52 -0500

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,14 +3,19 @@
 #include "dog.h"
 
 /**
-* print_dog - print the name of the file,
-*@d: structure dog.
+*new_dog - create new dog,
+*@name: name tu save.
+*@age: age to save.
+*@owner: owner to save.
 *Return: the return of finish file
 */
 
 dog_t *new_dog(char *name, float age, char *owner)
-{	
+{
 	struct dog *n;
+
+	if(name == NULL || owner == NULL)
+		return (NULL);
 	n = malloc(sizeof(struct dog));
 	if (n == NULL)
 		return (NULL);
