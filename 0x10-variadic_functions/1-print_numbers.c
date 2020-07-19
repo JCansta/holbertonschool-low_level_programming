f543d8c4b8bf4abc0443b305df1ecc595aef9e1f
Jaime Castro <jaime.castro03@icloud.com>
2020-07-19 01:13:15 -0500

    task 1-print_numbers.c better code

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,24 +14,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ptnumber;
 	unsigned int i;
 
+	va_start(ptnumber, n);
 	if (separator == NULL)
+		separator = "";
+	for (i = 0; i < n; i++)
 	{
-		va_start(ptnumber, n);
-		for (i = 0; i < n; i++)
-		{
-			printf("%d ", va_arg(ptnumber, unsigned int));
-		}
+		printf("%d", va_arg(ptnumber, unsigned int));
+		if (i != n - 1)
+			printf("%s", separator);
 	}
-	else
-	{
-		va_start(ptnumber, n);
-		for (i = 0; i < n; i++)
-		{
-			printf("%d", va_arg(ptnumber, unsigned int));
-			if (i != n - 1)
-				printf("%s", separator);
-		}
-	}
+
 	printf("\n");
 	va_end(ptnumber);
 }
