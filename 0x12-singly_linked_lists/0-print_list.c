1354d8c7d140cbf207494414a80e53c297b147b2
Jaime Castro <jaime.castro03@icloud.com>
2020-07-23 20:27:40 -0500

    task 0-print_lists.c and header file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ *print_list - print all elements of a list
+ *@h:
+ *Return: return the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int nod = 0, itemp;
+	char *temp;
+	while(h != NULL)
+	{
+		temp = h->str;
+		itemp = h->len;
+		if (temp == NULL)
+		{
+			temp = "(lib)";
+			itemp = 0;
+		}
+		printf("[%d] %s\n",itemp, temp);
+		h = h->next;
+		nod++;
+	}
+	return (nod);
+}
