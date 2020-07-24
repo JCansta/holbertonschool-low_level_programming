52b9dc7ba2c2a4a7c1027cb3c21bded7e798bdd8
Jaime Castro <jaime.castro03@icloud.com>
2020-07-23 20:35:19 -0500

    betty correction

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  *print_list - print all elements of a list
- *@h:
+ *@h: head of the file to move in the nodes
  *Return: return the number of nodes
  */
 
@@ -10,7 +10,8 @@ size_t print_list(const list_t *h)
 {
 	int nod = 0, itemp;
 	char *temp;
-	while(h != NULL)
+
+	while (h != NULL)
 	{
 		temp = h->str;
 		itemp = h->len;
@@ -19,7 +20,7 @@ size_t print_list(const list_t *h)
 			temp = "(lib)";
 			itemp = 0;
 		}
-		printf("[%d] %s\n",itemp, temp);
+		printf("[%d] %s\n", itemp, temp);
 		h = h->next;
 		nod++;
 	}
