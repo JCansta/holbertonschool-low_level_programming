e0694e4513509dda299d579d5f7fb226742ccd5e
Jaime Castro <jaime.castro03@icloud.com>
2020-07-25 10:14:08 -0500

    task 3-add_node_End.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *add_node - add a new node
+ *@head: the head of the node
+ *@str: the new string to enter
+ *Return: return the number of nodes
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+
+}
