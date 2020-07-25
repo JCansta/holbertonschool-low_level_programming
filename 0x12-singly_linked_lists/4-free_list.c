80a31590458090357b1a7edc1f46730d220051b2
Jaime Castro <jaime.castro03@icloud.com>
2020-07-25 15:01:01 -0500

    task 4-free_list.c correctly

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,23 +10,32 @@
 
 void free_list(list_t *head)
 {
-	list_t *current = head;
-	list_t *next_node;
 
 	if (head == NULL)
 	{
 		free(head);
 		return;
 	}
-	while (current->next != NULL)
+/*
+*	while (head != NULL)
+*	{
+*		free(head->str);
+*		free(head);
+*		head = head->next;
+*	}
+*/
+	list_t *current = head;
+	list_t *next_node;
+
+	while (current != NULL)
 	{
 
 		next_node = current->next;
+		free(current->str);
 		free(current);
 		current = next_node;
 
 	}
-	free(current);
 	next_node = NULL;
 	head = NULL;
 }
