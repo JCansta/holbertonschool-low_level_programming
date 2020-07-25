4db4527f4fe74fd9bd630280c7aabe18bf0a55c8
Jaime Castro <jaime.castro03@icloud.com>
2020-07-25 12:52:09 -0500

    task 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
- *add_node - add a new node
+ *add_node_end - add a new node at the end
  *@head: the head of the node
  *@str: the new string to enter
  *Return: return the number of nodes
@@ -10,5 +11,32 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *current = *head;
+	list_t *new;
+	char *c;
+	int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (current->next != NULL)
+			current = current->next;
+		current->next = new;
+	}
+	c = strdup(str);
+	while (c[i] != '\0')
+		i++;
+	new->str = c;
+	new->len = i;
+	new->next = NULL;
+	return (new);
 
 }
