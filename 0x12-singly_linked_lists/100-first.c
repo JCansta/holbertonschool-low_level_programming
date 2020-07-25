2a0e905315bfaecf75db849d9b1ae9556774e19e
Jaime Castro <jaime.castro03@icloud.com>
2020-07-25 17:02:10 -0500

    task 100 fixing incorrect output

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "lists.h"
 /**
  *before_main - write before main
  *Return: return the number of nodes
@@ -11,4 +12,3 @@ void before_main(void)
 printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
 		);
 }
-#include "lists.h"
