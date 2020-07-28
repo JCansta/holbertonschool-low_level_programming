87268be48ccee49f271f28210047c761ec15176b
Jaime Castro <jaime.castro03@icloud.com>
2020-07-28 13:26:08 -0500

    add 1-listing_len: return the number of elements in a linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *listint_len - print the numbers of every node
+ *@h: the head of the node
+ *Return: return the number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t i;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
