5dfb861b6adee86504c1ec5297d09831d0b28b1d
Jaime Castro <jaime.castro03@icloud.com>
2020-07-28 15:11:53 -0500

    task 4-free_listint.c: free the mallocs crated when create a node

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *free_listint - free the mallocs used to create new nodes
+ *@head: the head of the node
+ *Return: the address of new element
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *next;
+	while(head->next != NULL)
+	{
+		next = head->next;
+		free(head);
+		head = next;
+	}
+	free(head);
+}
