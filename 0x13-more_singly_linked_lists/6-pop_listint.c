b335276af7df01abbaa7d5ed9a7151cd107b6abf
Jaime Castro <jaime.castro03@icloud.com>
2020-07-28 15:58:46 -0500

    task 6-pop_listint.c: free the head

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *pop_listint - free the head of s linked list
+ *@head: the head of the node
+ *Return: the data inside n
+ */
+
+int pop_listint(listint_t **head)
+{
+	int save;
+	listint_t *current;
+	listint_t *next;
+
+	if (head == NULL)
+		return (0);
+	current = *head;
+	save = current->n;
+	next = current->next;
+	free(current);
+	*head = next;
+	return (save);
+}
