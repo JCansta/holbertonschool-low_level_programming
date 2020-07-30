7a5303c4a843bcb83962415fff6504a4e076a514
Jaime Castro <jaime.castro03@icloud.com>
2020-07-30 14:28:22 -0500

    task 0: convert binary string to a unsigned integer

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *binary_to_uint - convert binary to unsigned int.
+ *@b: values of arguments
+ *Return: unsigend int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0;
+	unsigned int n = 0, mult = 1;
+
+	while (b[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		if (b[i] == '1')
+		{
+			n += mult;
+		}
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		mult *= 2;
+		i--;
+	}
+	return (n);
+}
