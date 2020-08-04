abfacf6f4ab429de3fdbead4810a25a370e3f800
Jaime Castro <jaime.castro03@icloud.com>
2020-08-04 18:48:13 -0500

    task 1 fixing error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,11 +6,11 @@
  * @text_content: the amount of letters
  * Return: how many letters print
  */
-int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content)
 {
 	int df, len, fd2;
 
-	df = open(filename, 0_CREAT | 0_TRUNC | 0_WRONLY, 0600);
+	df = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (df == -1)
 		return (0);
 	if (text_content == NULL)
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content);
 		len++;
 	}
 	fd2 = write(df, text_content, len);
-	if (df2 == -1)
+	if (fd2 == -1)
 		return (-1);
 	close(df);
 	return (1);
