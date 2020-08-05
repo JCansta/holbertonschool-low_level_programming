6e180ac52e8f18d75d5a8a77d522513363f8f890
Jaime Castro <jaime.castro03@icloud.com>
2020-08-05 14:25:23 -0500

    task 0 fixing

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,27 +8,26 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
+	int fd, fdw;
 	char *buf;
 
 	if (letters == 0)
-		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
 		return (0);
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	fd = read(fd, buf, sizeof(char) * letters);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+	fd = read(fd, buf, letters);
 	if (fd == -1)
 		return (0);
-	fd = write(STDOUT_FILENO, buf, fd);
-	buf[fd - 1] = '\0';
-	if (fd == -1)
+	fdw = write(STDOUT_FILENO, buf, fd);
+	if (fdw == -1)
 		return (0);
 	close(fd);
 	free(buf);
-	return (fd);
+	return (fdw);
 }
