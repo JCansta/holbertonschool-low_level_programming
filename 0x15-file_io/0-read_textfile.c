f18cb5d96219361f949e65a20d07f4c52aca937f
Jaime Castro <jaime.castro03@icloud.com>
2020-08-08 13:03:18 -0500

    task 0 changing write byte size

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -26,7 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(buf);
 		return (0);
 	}
-	fdw = write(STDOUT_FILENO, buf, letters);
+	fdw = write(STDOUT_FILENO, buf, fdr);
 	if (fdw < 0)
 	{
 		free(buf);
