37dec4d8dd7415a2d47c21bb31456068872a7d5b
Jaime Castro <jaime.castro03@icloud.com>
2021-02-22 23:54:56 -0500

    island task

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/isr/bin/python3
+"""
+5-main
+"""
+def find_neight(tgrid, i, j):
+    neigth = 0
+    #left
+    if j > 0 and tgrid[i][j-1] is 1:
+        neigth += 1
+    #up
+    if i > 0 and tgrid[i-1][j] is 1:
+        neigth += 1
+    #down
+    if i < (len(tgrid) - 1) and tgrid[i+1][j] is 1:
+        neigth += 1
+
+    #right
+    if j < (len(tgrid[0]) - 1) and tgrid[i][j+1] is 1:
+        neigth += 1
+
+    return neigth
+
+
+def island_perimeter(grid):
+    '''define the perimeter of the island'''
+    perimeter = 0;
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j]:
+                perimeter += 4 - find_neight(grid, i, j)
+    return perimeter
